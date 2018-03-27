3b5efe9719cb98506dfc07ffb3de0fc409ed60db
Nucleartom <jacob.maarse@gmail.com>
2018-03-27 21:10:50 +0000

    tweet now refreshes upon being added, form clears when submitted if tweet is within length requirement and is not empty

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,22 +3,43 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+let data= [];
 
 $(document).ready(function () {
-    renderTweets(data);
+
+    loadTweets(renderTweets);
     $("form").on('submit', ajaxAddTweet)
+    
 });
+
+function loadTweets(cb) {
+    $.ajax({
+        url:'/tweets/',
+        method: 'GET',
+        success: function(incomingData) {
+            cb(incomingData);
+        }
+    })
+   
+}
 
 function ajaxAddTweet() {
     event.preventDefault();
-    $.ajax({
-        url: '/tweets/',
-        method: 'POST',
-        data: $(this).serialize(),
-        success: function() {
-            console.log('success');
-        }
-    })
+    if ($(this).find('textarea').val() === "") {
+        alert("Cannot send empty tweet");
+    } else if ($(this).find('textarea').val().length > 140) {
+        alert("Cannot tweet more than 140 characters");
+    } else {
+        $.ajax({
+            url: '/tweets/',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function () {
+                loadTweets(loadNewTweet);
+            }
+        })
+        $(this)[0].reset();
+    }
 };
 
 function renderTweets(tweetsArr) {
@@ -26,12 +47,15 @@ function renderTweets(tweetsArr) {
         createTweetElement(element);
     });
 };
+function loadNewTweet(tweetsArr) {
+    createTweetElement(tweetsArr[tweetsArr.length-1]);
+}
 
 function createTweetElement(tweetInfo) {
     let timeDiff = timeCalculator(tweetInfo.created_at);
-    let output = $(`<section id="tweet-container"><header class="tweet"><img class="avatar-pic" src=${tweetInfo.user.avatars.small}><div class="user">${tweetInfo.user.name}</div><div class="handle">${tweetInfo.user.handle}</div></header><article class="tweet">${tweetInfo.content.text}</article><footer class="tweet">${timeDiff}<img class="interactOptions" src="/images/interactOptions.png"></footer></section>`)
+    let output = $(`<section class="tweet-container"><header class="tweet"><img class="avatar-pic" src=${tweetInfo.user.avatars.small}><div class="user">${tweetInfo.user.name}</div><div class="handle">${tweetInfo.user.handle}</div></header><article class="tweet">${tweetInfo.content.text}</article><footer class="tweet">${timeDiff}<img class="interactOptions" src="/images/interactOptions.png"></footer></section>`)
 
-    $('.container').append(output);
+    $('#feed-container').prepend(output);
 };
 
 
@@ -94,49 +118,50 @@ function timeCalculator(created) {
     }
 };
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//           "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//         },
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+//           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+//           "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+//         },
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     },
+//     {
+//       "user": {
+//         "name": "Johann von Goethe",
+//         "avatars": {
+//           "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+//           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+//           "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+//         },
+//         "handle": "@johann49"
+//       },
+//       "content": {
+//         "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+//       },
+//       "created_at": 1461113796368
+//     }
+//   ];
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
     $("textarea").on("paste keyup", function (event) {
         let count = $(this).val().length
         let counterVal = 140 - count;
-        console.log(count, counterVal)
         if (counterVal >= 0) {
             $(this).siblings('.counter').text(counterVal).removeClass(' invalidLength');
         } else {
             $(this).siblings('.counter').text(counterVal).addClass(' invalidLength');
         }
-        console.log(event);
     });
 });
