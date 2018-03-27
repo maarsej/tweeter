d49671f51f6988447590f47cee0dcb21a4ec5dc5
Nucleartom <jacob.maarse@gmail.com>
2018-03-27 00:11:12 +0000

    tried to modify so that user could not trick the counter with pasting into the field

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
-    // console.log('properly linked');
-    // $("textarea").on("keyup", (event) => console.log(event.key));
-    // $("textarea").on("keypress", (event) => console.log(event));
-
-    $("textarea").on("keyup", function (event) {
-        // console.log(event);
-        let count = $(this).val().length
+    $("textarea").on("paste keyup", function (event) {
+        let count = event.currentTarget.value.length
         let counterVal = 140 - count;
+        console.log(count, counterVal)
         if (counterVal >= 0) {
             $(this).siblings('.counter').text(counterVal).removeClass(' invalidLength');
         } else {
             $(this).siblings('.counter').text(counterVal).addClass(' invalidLength');
         }
-        console.log(counterVal);
+        console.log(event);
     });
 });
